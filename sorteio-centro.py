2b9b3a0cf10751a0232df00091a987e52f2ca502
Thayna Francisco <42574298+thaynafrancisco@users.noreply.github.com>
2023-09-18 21:02:58 -0300

    working but no ready

--- sorteio-centro.py
+++ sorteio-centro.py
@@ -1,99 +1,51 @@
 import random
-import csv
 
 # Lista de pessoas
-pessoas = [
+nomes = [
     "Alexsandro", "Maah", "Charles", "Guilherme", "Bianca", "Everton", "Franciely",
     "Deziree", "Maria Paula", "Tatiane", "Du", "Gaby", "Domenico", "Gabi Gonzaga",
     "Thais Takeushi", "Thais Miliao", "Bryan", "Shey", "Felipe", "Pedro Barreto",
-    "Pronto E.", "Mi", "Paula", "Vitor", "Nicoly", "Maria - Nova", "Paulo - Novo"
+    "Pronto E.", "Mi", "Paula", "Vitor", "Nicoly", "Maria", "Paulo"
 ]
 
-# Definir os setores
-setores = {
-    "Assistência": 0,
-    "Senha": 0,
-    "Álcool": 0,
-    "Lago": 0,
-    "Cozinha": 0,
-    "Setor 1": 0,
-    "Setor 2": 0,
-    "Setor 3": 0,
-    "Setor 4": 0,
-    "Casinhas": 0,
-    "Criancas": 0,
-    "Sala de Cambone": 0
+pessoas = 25
+
+divisao = {
+    "Assistência": 1,
+    "Senha": 1,
+    "Álcool": 1,
+    "Lago": 1,
+    "Cozinha": 2,
+    "Setor 1": 4,
+    "Setor 2": 4,
+    "Setor 3": 4,
+    "Setor 4": 4,
+    "Casinhas": 1,
+    "Criancas": 1,
+    "Sala de Cambone": 1
 }
 
-# Dicionário para controlar quantas vezes cada pessoa ficou em cada setor
-registro = {pessoa: {setor: 0 for setor in setores} for pessoa in pessoas}
+# Calcula o número total de vagas disponíveis
+vagas_disponiveis = sum(divisao.values())
 
-# Carregar o registro existente (se houver)
-try:
-    with open("registro.csv", "r") as arquivo_csv:
-        leitor_csv = csv.DictReader(arquivo_csv)
-        for linha in leitor_csv:
-            pessoa = linha["Pessoa"]
-            setor = linha["Setor"]
-            vezes = int(linha["Vezes"])
-            registro[pessoa][setor] = vezes
-except FileNotFoundError:
-    pass
+# Verifica se o número total de vagas é suficiente para acomodar todas as pessoas
+if vagas_disponiveis < pessoas:
+    print("Não há vagas suficientes para acomodar todas as pessoas.")
+else:
+    # Distribui aleatoriamente as pessoas nas categorias
+    random.shuffle(nomes)
+    distribuicao = {categoria: [] for categoria in divisao.keys()}
+    for categoria, vagas in divisao.items():
+        for _ in range(vagas):
+            if nomes:
+                pessoa = nomes.pop()
+                distribuicao[categoria].append(pessoa)
 
-def atribuir_pessoa_ao_setor(pessoa):
-    """Atribui uma pessoa a um setor, evitando repetições frequentes."""
-    while True:
-        setor = random.choice(list(setores.keys()))
-        if setor in ["Lago", "Casinhas", "Álcool", "Senha", "Assistência", "Criancas"]:
-            if setores[setor] == 0:
-                setores[setor] += 1
-                return setor
-        elif setor == "Cozinha":
-            if setores[setor] < 2:
-                setores[setor] += 1
-                return setor
-        elif setor in ["Setor 1", "Setor 2", "Setor 3", "Setor 4"]:
-            if setores[setor] >= 4:
-                continue
-            # Verificar quantas vezes a pessoa ficou no setor
-            vezes_no_setor = registro[pessoa][setor]
-            if vezes_no_setor >= 3:  # Evite mais de 3 vezes no mesmo setor
-                continue
-            setores[setor] += 1
-            registro[pessoa][setor] = vezes_no_setor + 1
-            return setor
-        elif setor == "Sala de Cambone":
-            if pessoa in ["Maria - Nova", "Paulo - Novo"]:
-                setores[setor] += 1
-                return setor
+    # Distribui as pessoas restantes na "Sala de Cambone"
+    while nomes:
+        pessoa = nomes.pop()
+        distribuicao["Sala de Cambone"].append(pessoa)
 
-def main():
-    """Função principal para atribuir pessoas aos setores e imprimir os resultados."""
-    # Embaralhar a lista de pessoas aleatoriamente
-    random.shuffle(pessoas)
-
-    # Inicializar um dicionário para armazenar as atribuições
-    atribuicoes = {setor: [] for setor in setores}
-
-    # Atribuir pessoas aos setores
-    for pessoa in pessoas:
-        setor_atribuido = atribuir_pessoa_ao_setor(pessoa)
-        atribuicoes[setor_atribuido].append(pessoa)
-
-    # Imprimir as atribuições
-    for setor, pessoas_atribuidas in atribuicoes.items():
-        print(f"{setor}: {pessoas_atribuidas}")
-
-    # Salvar o registro atualizado no arquivo CSV
-    with open("registro.csv", "w", newline="") as arquivo_csv:
-        campos = ["Pessoa", "Setor", "Vezes"]
-        escritor_csv = csv.DictWriter(arquivo_csv, fieldnames=campos)
-        escritor_csv.writeheader()
-        for pessoa, setores_em_que_ficou in registro.items():
-            for setor, vezes in setores_em_que_ficou.items():
-                escritor_csv.writerow({"Pessoa": pessoa, "Setor": setor, "Vezes": vezes})
-
-    print("Registro atualizado e salvo.")
-
-if __name__ == "__main__":
-    main()
+    # Imprime a distribuição
+    for categoria, pessoas_na_categoria in distribuicao.items():
+        print(f"{categoria}: {', '.join(pessoas_na_categoria)}")
